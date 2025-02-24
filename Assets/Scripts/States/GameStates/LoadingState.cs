4f70c66301c222f7544b1f287105f08c4d300dfe
Olcay Seygan <seyganolcay@gmail.com>
2025-02-24 21:51:55 +0300

    feat: update default solution name, remove unused linker file, and enhance LoadingState with IronSource ad events

--- Assets/Scripts/States/GameStates/LoadingState.cs
+++ Assets/Scripts/States/GameStates/LoadingState.cs
@@ -17,12 +17,20 @@ namespace Assets.Scripts.States.GameStates
             Game.I.StateViewHandler.SwitchTo<LoadingPanel>();
             QualitySettings.vSyncCount = 0;
             Application.targetFrameRate = PlayerController.I.GetMaxFPS();
-
+            IronSource.Agent.shouldTrackNetworkState(true);
+            IronSourceRewardedVideoEvents.onAdOpenedEvent += RewardedVideoOnAdOpenedEvent;
+            IronSourceRewardedVideoEvents.onAdClosedEvent += RewardedVideoOnAdClosedEvent;
+            IronSourceRewardedVideoEvents.onAdAvailableEvent += RewardedVideoOnAdAvailable;
+            IronSourceRewardedVideoEvents.onAdUnavailableEvent += RewardedVideoOnAdUnavailable;
+            IronSourceRewardedVideoEvents.onAdShowFailedEvent += RewardedVideoOnAdShowFailedEvent;
+            IronSourceRewardedVideoEvents.onAdRewardedEvent += RewardedVideoOnAdRewardedEvent;
+            IronSourceRewardedVideoEvents.onAdClickedEvent += RewardedVideoOnAdClickedEvent;
             LevelPlayAdFormat[] legacyAdFormats = new[] { LevelPlayAdFormat.REWARDED };
             Debug.Log("LoadingState");
             LevelPlay.OnInitSuccess += (adFormats) =>
             {
                 Debug.Log("LevelPlay initialized");
+                IronSource.Agent.loadRewardedVideo();
             };
             LevelPlay.OnInitFailed += (error) =>
             {
@@ -30,13 +38,13 @@ namespace Assets.Scripts.States.GameStates
                 Debug.Log(error);
             };
             IronSource.Agent.setMetaData("is_test_suite", "enable");
-            LevelPlay.Init("2103ecc85", adFormats: legacyAdFormats);
 
-            IronSourceEvents.onSdkInitializationCompletedEvent += () =>
+            IronSourceRewardedVideoEvents.onAdAvailableEvent += (adInfo) =>
             {
-                Debug.Log("IronSource initialized");
-                IronSource.Agent.launchTestSuite();
+                Debug.Log("IronSource rewarded video available");
+                Debug.Log(adInfo);
             };
+            LevelPlay.Init("2103ecc85", adFormats: legacyAdFormats);
             AudioManager.I.LoadAudioSettings();
             // MobileAds.Initialize(initStatus =>
             // {
@@ -63,5 +71,46 @@ namespace Assets.Scripts.States.GameStates
             yield return new WaitForSeconds(1f);
             self.StateProvider.SwitchTo<InstantiationState>(new InstantiationStateProperties() { canSkipToPlaying = false });
         }
+
+
+        /************* RewardedVideo AdInfo Delegates *************/
+        // Indicates that there’s an available ad.
+        // The adInfo object includes information about the ad that was loaded successfully
+        // This replaces the RewardedVideoAvailabilityChangedEvent(true) event
+        void RewardedVideoOnAdAvailable(IronSourceAdInfo adInfo)
+        {
+            Debug.Log("Rewarded video ad available");
+            Debug.Log(adInfo.adUnit);
+        }
+        // Indicates that no ads are available to be displayed
+        // This replaces the RewardedVideoAvailabilityChangedEvent(false) event
+        void RewardedVideoOnAdUnavailable()
+        {
+        }
+        // The Rewarded Video ad view has opened. Your activity will loose focus.
+        void RewardedVideoOnAdOpenedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // The Rewarded Video ad view is about to be closed. Your activity will regain its focus.
+        void RewardedVideoOnAdClosedEvent(IronSourceAdInfo adInfo)
+        {
+        }
+        // The user completed to watch the video, and should be rewarded.
+        // The placement parameter will include the reward data.
+        // When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback.
+        void RewardedVideoOnAdRewardedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
+        {
+        }
+        // The rewarded video ad was failed to show.
+        void RewardedVideoOnAdShowFailedEvent(IronSourceError error, IronSourceAdInfo adInfo)
+        {
+        }
+        // Invoked when the video ad was clicked.
+        // This callback is not supported by all networks, and we recommend using it only if
+        // it’s supported by all networks you included in your build.
+        void RewardedVideoOnAdClickedEvent(IronSourcePlacement placement, IronSourceAdInfo adInfo)
+        {
+        }
+
     }
 }
